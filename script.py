575ebe540740ff368aacc28c556d9c585b31eb1b
daletal <fayaca@gmail.com>
2019-07-15 02:48:34 +0200

    tensor 001

--- script.py
+++ script.py
@@ -1,25 +1,69 @@
-import math
-import sys
-from os import rename
+import tensorflow as tf
 
-import requests
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0)  # 預設型別亦為 tf.float32
+print(node1, node2)
 
-print(sys.version)
-print(sys.executable)
+sess = tf.Session()
+print(sess.run([node1, node2]))
+
+node3 = tf.add(node1, node2)
+print("node3: ", node3)
+print("sess.run(node3): ", sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # 加號（+）等同於 tf.add(a, b) 的效果
+
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+add_and_triple = adder_node * 3.0
+print(sess.run(add_and_triple, {a: 3, b: 4.5}))
+
+W = tf.Variable([0.3], dtype=tf.float32)
+b = tf.Variable([-0.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+
+y = tf.placeholder(tf.float32)
+
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+fixW = tf.assign(W, [-1.0])
+fixb = tf.assign(b, [1.0])
+sess.run([fixW, fixb])
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+# import math
+# import sys
+# from os import rename
+
+# import requests
+
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
-r = requests.get("https://www.google.com")
-print(r.status_code)
+# r = requests.get("https://www.google.com")
+# print(r.status_code)
 
-name = input("your name?")
+# name = input("your name?")
 # print("hello,", name)
 # print("go good job")
 # print("test2")
 # print("test03")
-print("test04")
+# print("test04")
 # print("test05")
